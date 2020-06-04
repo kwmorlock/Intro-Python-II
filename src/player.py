486666a9e4f250b4dd71f58c37989cc0d15bd2b2
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-06-03 20:13:02 -0400

    inventory added to player class

--- src/player.py
+++ src/player.py
@@ -1,6 +1,10 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 class Player():
-    def __init__(self, name, current_room):
+    def __init__(self, name, current_room, inventory):
         self.name = name
         self.current_room = current_room
+        if inventory == []:
+            self.inventory = []
+        else:
+            self.inventory = inventory
